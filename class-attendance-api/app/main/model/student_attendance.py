325c106530e8059b99f9944cfdfe063d54bd2cac
Ubuntu <ubuntu@ip-172-31-43-181.eu-central-1.compute.internal>
2020-01-01 03:59:33 +0000

    Beanstalk stable

--- class-attendance-api/app/main/model/student_attendance.py
+++ class-attendance-api/app/main/model/student_attendance.py
@@ -3,4 +3,4 @@ from .. import db
 class StudentAttendance(db.Model):
     __tablename__ = 'student_attendance'
     student_id = db.Column(db.String, db.ForeignKey('student.student_id'),  primary_key=True)
-    attendance_id = db.Column(db.Integer, db.ForeignKey('attendance.public_id'), primary_key=True)
+    attendance_id = db.Column(db.String, db.ForeignKey('attendance.public_id'), primary_key=True)
