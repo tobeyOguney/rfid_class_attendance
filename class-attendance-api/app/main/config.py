4f791257f79b5d93ac1ffdd6be14061ec3418d46
tobeyOguney <tobyogunbayo@gmail.com>
2020-01-01 05:07:43 +0100

    Update config.py

--- class-attendance-api/app/main/config.py
+++ class-attendance-api/app/main/config.py
@@ -1,5 +1,14 @@
 import os
 
+
+postgres_params = {
+    'NAME': os.environ['RDS_DB_NAME'],
+    'USER': os.environ['RDS_USERNAME'],
+    'PASSWORD': os.environ['RDS_PASSWORD'],
+    'HOST': os.environ['RDS_HOSTNAME'],
+    'PORT': os.environ['RDS_PORT'],
+}
+
 # uncomment the line below for postgres database url from environment variable
 postgres_local_base = "postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}".format(**postgres_params)
 
