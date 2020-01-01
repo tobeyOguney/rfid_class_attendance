c8b56986f7ae54cd1bdc77108576c8e6a2c592f4
tobeyOguney <tobyogunbayo@gmail.com>
2020-01-01 05:04:41 +0100

    Update config.py

--- class-attendance-api/app/main/config.py
+++ class-attendance-api/app/main/config.py
@@ -13,9 +13,9 @@ class Config:
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
